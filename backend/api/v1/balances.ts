f3c45f143d269404cb15196717fe555f79c7b65a
Jem <0x0xjem@gmail.com>
2022-03-17 17:30:46 +0400

    Missing closing bracket

--- backend/api/v1/balances.ts
+++ backend/api/v1/balances.ts
@@ -10,7 +10,7 @@ const balances = async (request: VercelRequest, response: VercelResponse) => {
   const { address, chainId } = request.query as Query;
 
   const responseData = await tokensBalances(address, parseInt(chainId)).catch((e) => {
-    console.log(`/api/v1/balances GET error: ${e.message} (address: ${address}`);
+    console.log(`/api/v1/balances GET error: ${e.message} (address: ${address})`);
     response.status(400).json(e.message);
   });
   response.status(200).json(responseData);
